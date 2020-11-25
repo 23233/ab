c21a6a344b77a247c0a46efb512a6e82706f726e
23233 <1@qq.com>
2020-11-25 12:50:31 +0800

    新增测试

--- _examples/index_test.go
+++ _examples/index_test.go
@@ -10,7 +10,7 @@ func TestNewApp(t *testing.T) {
 	e := httptest.New(t, app)
 	prefix := "/api/v1"
 	testModel := "test_model_a"
-	// 测试新增
+	// 测试新增 不支持withjson 因为iris只有readJson 没有单个json fields的获取
 	bodyMap := map[string]interface{}{"name": "test"}
 	addData := e.POST(prefix + "/" + testModel).WithForm(bodyMap).Expect().Status(httptest.StatusOK)
 	addData.JSON().Object().Empty()
