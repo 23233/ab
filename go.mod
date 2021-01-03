24ea4a0666c7e0d0fd656a0fd4b4299c79adf07d
1 <1@qq.com>
2021-01-03 20:48:13 +0800

    1.new version loading....

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/23233/sv v1.1.2
+	github.com/go-redis/redis/v8 v8.4.4 // indirect
 	github.com/gomodule/redigo v1.8.2 // indirect
 	github.com/json-iterator/go v1.1.10
 	github.com/kataras/iris/v12 v12.2.0-alpha.0.20201117050536-962ffd67721a
--- short.go
+++ short.go
@@ -1,40 +1,78 @@
 package ab
 
 import (
+	"github.com/go-redis/redis/v8"
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/context"
+	"time"
 	"xorm.io/xorm"
 )
 
 type SingleModel struct {
-	Middlewares       []context.Handler
-	Prefix            string // 路由前缀
-	Suffix            string // 路由后缀
-	Model             interface{}
-	EnablePrivate     bool                   // 启用私密模型
-	PrivateContextKey string                 // 上下文key
-	PrivateColName    string                 // 数据库字段名
-	DisableMethods    []string               // get(all) get(single) post put delete
-	SearchFields      []string               // 搜索的字段 struct名称
-	GetAllFunc        func(ctx iris.Context) // 覆盖获取全部方法
-	GetAllResponse    interface{}            // 获取所有返回的内容替换 仅替换data数组 同名替换
-	GetSingleFunc     func(ctx iris.Context) // 覆盖获取单条方法
-	GetSingleResponse interface{}            // 获取单个返回的内容替换
-	PostFunc          func(ctx iris.Context) // 覆盖新增方法
-	PostValidator     interface{}            // 新增自定义验证器
-	PostResponse      interface{}            // 新增返回内容
-	PutFunc           func(ctx iris.Context) // 覆盖修改方法
-	PutValidator      interface{}            // 修改验证器
-	PutResponse       interface{}            // 修改返回内容
-	DeleteFunc        func(ctx iris.Context) // 覆盖删除方法
-	DeleteValidator   interface{}            // 删除验证器
-	DeleteResponse    interface{}            // 删除返回内容
+	Middlewares        []context.Handler
+	Prefix             string                 // 路由前缀
+	Suffix             string                 // 路由后缀
+	Model              interface{}            // xorm model
+	PrivateContextKey  string                 // 上下文key
+	PrivateColName     string                 // 数据库字段名
+	ExtraFilters       map[string]interface{} //
+	EnableMethods      []string               //
+	DisableMethods     []string               // get(all) get(single) post put delete
+	SearchFields       []string               // 搜索的字段 struct名称
+	GetAllFunc         func(ctx iris.Context) // 覆盖获取全部方法
+	GetAllResponse     interface{}            // 获取所有返回的内容替换 仅替换data数组 同名替换
+	GetSingleFunc      func(ctx iris.Context) // 覆盖获取单条方法
+	GetSingleResponse  interface{}            // 获取单个返回的内容替换
+	PostFunc           func(ctx iris.Context) // 覆盖新增方法
+	PostValidator      interface{}            // 新增自定义验证器
+	PostResponse       interface{}            // 新增返回内容
+	PutFunc            func(ctx iris.Context) // 覆盖修改方法
+	PutValidator       interface{}            // 修改验证器
+	PutResponse        interface{}            // 修改返回内容
+	DeleteFunc         func(ctx iris.Context) // 覆盖删除方法
+	DeleteValidator    interface{}            // 删除验证器
+	DeleteResponse     interface{}            // 删除返回内容
+	CacheTime          time.Time              //
+	GetAllCacheTime    time.Time              //
+	GetSingleCacheTime time.Time              //
+}
+
+type MysqlConfig struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+	DbName   string
+	PoolSize int
+}
+
+type RedisConfig struct {
+	Host     string
+	Port     int
+	Password string
+	Db       int
+	PoolSize int
+}
+
+type IrisInstance struct {
+	Party iris.Party
+	App   iris.Application
+}
+
+type MysqlInstance struct {
+	MysqlConfig
+	Mdb *xorm.Engine
+}
+
+type RedisInstance struct {
+	RedisConfig
+	Rdb *redis.Client
 }
 
 type Config struct {
 	Party      iris.Party
+	App        iris.Application
 	StructList []SingleModel
-	Engine     *xorm.Engine
 }
 
 type respItem struct {
